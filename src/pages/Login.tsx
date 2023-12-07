f937097ca3228910b3d40e547b98494b2f1d509e
Abdul Basit <beingabdul10@gmail.com>
2023-12-07 10:09:08 +0500

    Login Page Progress

---
+++ src/pages/Login.tsx
@@ -0,0 +1,12 @@
+import { Calendar } from "@progress/kendo-react-dateinputs"
+import kendoka from '../kendoka.svg';
+
+function Login (){
+    return <div className="App">
+    <header className="App-header">
+        <h2>Login</h2>
+      <img src={kendoka} className="App-logo" alt="kendoka" />
+    </header>
+  </div>
+}
+export default Login
